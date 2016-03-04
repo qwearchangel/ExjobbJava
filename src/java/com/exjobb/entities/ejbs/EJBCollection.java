2f3a6b1604817e6921db5d333c788b6e15ddab7d
Filip Ferm <filip.ferm@starrepublic.com>
2016-03-04 16:21:12 +0100

    EJBCollection
    Finnished EJBColleciton class

--- src/java/com/exjobb/entities/ejbs/EJBCollection.java
+++ src/java/com/exjobb/entities/ejbs/EJBCollection.java
@@ -5,10 +5,32 @@
  */
 package com.exjobb.entities.ejbs;
 
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+
 /**
  *
  * @author Filip
  */
+@Stateless
 public class EJBCollection {
     
+    @EJB
+    CategoryManager cm;
+    @EJB
+    ProductManager pm;
+    @EJB
+    ItemManager im;
+    
+    public CategoryManager category() {
+        return cm;
+    }
+    
+    public ProductManager product() {
+        return pm;
+    }
+    
+    public ItemManager item() {
+        return im;
+    }
 }
