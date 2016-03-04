6e38ce7152d7a91c9c6711dd61506d60ad0bf871
Filip Ferm <filip.ferm@starrepublic.com>
2016-03-04 15:57:46 +0100

    commit
    commit

---
+++ src/java/com/exjobb/entities/models/Item.java
@@ -0,0 +1,141 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.exjobb.entities.models;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Filip
+ */
+@Entity
+@Table(name = "item")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Item.findAll", query = "SELECT i FROM Item i"),
+    @NamedQuery(name = "Item.findById", query = "SELECT i FROM Item i WHERE i.id = :id"),
+    @NamedQuery(name = "Item.findByColor", query = "SELECT i FROM Item i WHERE i.color = :color"),
+    @NamedQuery(name = "Item.findBySize", query = "SELECT i FROM Item i WHERE i.size = :size"),
+    @NamedQuery(name = "Item.findByFeetsize", query = "SELECT i FROM Item i WHERE i.feetsize = :feetsize"),
+    @NamedQuery(name = "Item.findByImagepath", query = "SELECT i FROM Item i WHERE i.imagepath = :imagepath")})
+public class Item implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Size(max = 45)
+    @Column(name = "color")
+    private String color;
+    @Size(max = 45)
+    @Column(name = "size")
+    private String size;
+    @Size(max = 45)
+    @Column(name = "feetsize")
+    private String feetsize;
+    @Size(max = 255)
+    @Column(name = "imagepath")
+    private String imagepath;
+    @JoinColumn(name = "Product_id", referencedColumnName = "id")
+    @ManyToOne(optional = false)
+    private Product productid;
+
+    public Item() {
+    }
+
+    public Item(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getFeetsize() {
+        return feetsize;
+    }
+
+    public void setFeetsize(String feetsize) {
+        this.feetsize = feetsize;
+    }
+
+    public String getImagepath() {
+        return imagepath;
+    }
+
+    public void setImagepath(String imagepath) {
+        this.imagepath = imagepath;
+    }
+
+    public Product getProductid() {
+        return productid;
+    }
+
+    public void setProductid(Product productid) {
+        this.productid = productid;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Item)) {
+            return false;
+        }
+        Item other = (Item) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.exjobb.entities.models.Item[ id=" + id + " ]";
+    }
+    
+}
